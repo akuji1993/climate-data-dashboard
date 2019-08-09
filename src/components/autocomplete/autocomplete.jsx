102b0ab3a0d7d365d6028f9a523b801a432e2f48
Tobias Arweiler <tobias.arweiler@ergosign.de>
2019-08-09 13:22:24 +0200

    Fixed autocomplete component and styled it

--- src/components/autocomplete/autocomplete.jsx
+++ src/components/autocomplete/autocomplete.jsx
@@ -1,54 +1,65 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import style from "./autocomplete.module.scss";
 import PropTypes from "proptypes";
 
-const AutoComplete = props => {
-  const [inputValue, setInputValue] = useState("");
-  const [filteredOptions, setFilteredOptions] = useState([]);
+class AutoComplete extends React.Component {
+  constructor() {
+    super();
 
-  useEffect(() => {
-    const { options } = props;
-    return () => {
-      if (inputValue.length > 3) {
-        setFilteredOptions(
-          options.filter(
-            opt =>
-              opt.label.toLowerCase().indexOf(inputValue.toLowerCase()) > -1
-          )
-        );
-      }
+    this.handleChange = this.handleChange.bind(this);
+    this.onSuggestionClick = this.onSuggestionClick.bind(this);
+
+    this.state = {
+      inputValue: "",
+      filteredOptions: []
     };
-  }, [inputValue, props]);
+  }
 
-  const onSuggestionClick = selectedOption => {
-    const { onChange } = props;
-    setInputValue("");
-    setFilteredOptions([]);
+  handleChange(inputValue) {
+    const { options } = this.props;
+    this.setState({
+      inputValue,
+      filteredOptions: options.filter(
+        opt => opt.label.toLowerCase().indexOf(inputValue.toLowerCase()) > -1
+      )
+    });
+  }
+
+  onSuggestionClick(selectedOption) {
+    const { onChange } = this.props;
+    this.setState({
+      inputValue: "",
+      filteredOptions: []
+    });
     onChange(selectedOption.code);
-  };
+  }
 
-  return (
-    <div className={style.AutoComplete}>
-      <input
-        type="text"
-        value={inputValue}
-        onChange={event => setInputValue(event.target.value)}
-      />
-      {inputValue.length > 3 && (
-        <div className={style.suggestions}>
-          {filteredOptions.map((opt, idx) => (
-            <div
-              onClick={() => onSuggestionClick(opt)}
-              key={`suggestion-${opt.code}-${idx}`}
-              className={style.suggestion}>
-              {opt.label}
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
-};
+  render() {
+    const { inputValue, filteredOptions } = this.state;
+    return (
+      <div className={style.AutoComplete}>
+        <input
+          className={style.input}
+          type="text"
+          value={inputValue}
+          onChange={event => this.handleChange(event.target.value)}
+        />
+        {inputValue.length > 3 && (
+          <div className={style.suggestions}>
+            {filteredOptions.map((opt, idx) => (
+              <div
+                onClick={() => this.onSuggestionClick(opt)}
+                key={`suggestion-${opt.code}-${idx}`}
+                className={style.suggestion}>
+                {opt.label}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
 
 AutoComplete.propTypes = {
   options: PropTypes.arrayOf(
